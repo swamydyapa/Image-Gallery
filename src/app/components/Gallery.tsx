8ef77319ce2cf17f815f1783466edf876345eed5
swamydyapa <swamydyapa6.webdevp8@gmail.com>
2023-12-21 01:44:03 +0530

    changed layout to image grid

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -19,7 +19,7 @@ export default async function Gallery({ topic }: Props) {
 
 	const photosWithBlur = await addBlurredDataUrls(images);
 	return (
-		<section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+		<section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
 			{photosWithBlur.map((photo) => (
 				<ImgContainer photo={photo} key={photo.id} />
 			))}
